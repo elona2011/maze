39cfe24f0b1502f4ca6aef342f3368f1fc94cc9a
elona2011 <cdszbjj@qq.com>
2017-07-09 19:51:53 +0800

    u

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
-let table = `<table>${getArea('40*30')}</table>`
+let obstacle = {},
+    used = {},
+    mousedown = false,
+    width = 40,
+    height = 30
+
+let table = `<table>${getArea(width+'*'+height)}</table>`
 let area = document.createRange().createContextualFragment(table)
 document.body.appendChild(area)
-setStep()
+document.querySelector('table').addEventListener('click', e => {
+    if (e.target.matches('td')) {
+        obstacle[e.target.dataset.x + ',' + e.target.dataset.y] = 1
+        e.target.classList.add('obstacle')
+    }
+})
+window.addEventListener('mousedown', e => {
+    mousedown = true
+}, false)
+window.addEventListener('mouseup', e => {
+    mousedown = false
+}, false)
+document.querySelector('table').addEventListener('mousemove', e => {
+    if (mousedown) {
+        obstacle[e.target.dataset.x + ',' + e.target.dataset.y] = 1
+        e.target.classList.add('obstacle')
+    }
+}, false)
+document.querySelector('button').addEventListener('click', e => {
+    used[0 + ',' + 0] = 1
+    writeNum([0, 0], 0)
+    let now = [],
+        step = 0
 
-function setStep(target = [0, 0]) {
-    writeNum([0, 0], 3)
+    now.push([0, 0])
+    while (now.length) {
+        let r = now.shift()
+        setStep({
+            x: r[0],
+            y: r[1],
+            step: r[2],
+            now
+        })
+    }
+}, false)
+
+function setStep({
+    x = 0,
+    y = 0,
+    step = 0,
+    now
+}) {
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1 + Math.abs(i - x); j <= y + 1 - Math.abs(i - x); j++) {
+            if (isValid(i, j)) {
+                now.push([i, j, step + 1])
+                used[i + ',' + j] = 1
+                writeNum([i, j], step + 1)
+            }
+        }
+    }
 }
 
 function getAllPositionForStep(target = [0, 0], step) {
 
 }
 
+function isValid(x, y) {
+    if (x < 0 || y < 0 || x > width - 1 || y > height - 1) return false
+
+    if (x + ',' + y in obstacle) return false
+    if (x + ',' + y in used) return false
+
+    return true
+}
+
 function writeNum([x, y], num) {
-    let td = document.querySelector(`tr:nth-child(${x+1})>td:nth-child(${y+1})`)
+    let td = document.querySelector(`tr:nth-child(${y+1})>td:nth-child(${x+1})`)
     td.textContent = num
 }
 
 function getArea(range) {
     let [x, y] = range.split('*')
-    let td = '',
-        tr = ''
-    for (let i = 0; i < x; i++) {
-        td += `<td width="20" height="20"></td>`
-    }
+    let tr = ''
     for (let i = 0; i < y; i++) {
+        let td = ''
+        for (let j = 0; j < x; j++) {
+            td += `<td width="20" height="20" data-x="${j}" data-y="${i}"></td>`
+        }
         tr += `<tr>${td}</tr>`
     }
     return tr
